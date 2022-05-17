10952f1f0ee8e4107b61b6d262793e041941c500
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-17 18:12:17 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,19 @@
+"""
+Name: Karthik
+Date: 17th May 2022
+Day: Tuesday
+Time: 6:00-7:00PM
+File Description:  Giving values
+"""
+# a=input("Enter value of a:")
+# b=input("Enter value of b:")
+# print("Concatination of a and b is:",a+b)
+
+# a=float(input("Enter the value of a:"))
+# b=float(input("Enter the value of b:"))
+# print("value of a and b is:",a+b)
+
+# A1=int(input("Enter the value of A1:"))
+# A2=int(input("Enter the value of A2:"))
+# print("Addition of A1 and A2 is:",A1+A2)
+
